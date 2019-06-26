59905ac36ebdef8571b4818269fd5df4cedfb0e8
Maknollram <maknollram@hotmail.com>
2019-06-26 13:58:06 -0300

    mod

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -42,7 +42,7 @@ class FileController{
 
         await File.deleteOne( {_id: fileId})
 
-        req.io.sockets.in(box._id).emit('file')
+        req.io.sockets.in(box._id).emit('file',file)
 
     }
 
